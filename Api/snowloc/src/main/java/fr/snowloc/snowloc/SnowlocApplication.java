7e894cf7a26c384b62b785679f9900485f8abdfc
dgoult <dylan.goult@limayrac.fr>
2023-03-14 21:20:48 +0100

    initial

---
+++ Api/snowloc/src/main/java/fr/snowloc/snowloc/SnowlocApplication.java
@@ -0,0 +1,13 @@
+package fr.snowloc.snowloc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnowlocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnowlocApplication.class, args);
+	}
+
+}
---
+++ Api/snowloc/src/test/java/fr/snowloc/snowloc/SnowlocApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.snowloc.snowloc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnowlocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
